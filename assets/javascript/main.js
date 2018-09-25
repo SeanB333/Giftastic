436d43afbeb6809b0eaaf2ce4dfb460292310e7b
SeanB333 <spb4578@gmail.com>
2018-09-25 12:54:27 -0500

    fixing pause issues

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -15,10 +15,13 @@ $(document).ready(function () {
       for (var i = 0; i < gifPull.length; i++) {
         if (gifPull[i].rating !== "r" && gifPull[i].rating !== "pg-13") {
           var gifDiv = $("<div>");
+          gifDiv.addClass("float-left m-2 rounded");
           var rating = gifPull[i].rating;
           var p = $("<p>").text("Rating: " + rating);
+          p.addClass("text-light");
           var personImage = $("<img>");
           personImage.attr("src", gifPull[i].images.fixed_height.url);
+          personImage.addClass("rounded")
           gifDiv.append(p);
           gifDiv.append(personImage);
           $("#gifsPull").prepend(gifDiv);
@@ -33,6 +36,7 @@ $(document).ready(function () {
     for (i = 0; i < gitButtons.length; i++) {
       var btn = $("<button>");
       btn.addClass("newBtn");
+      btn.addClass("btn btn-dark m-2")
       btn.attr("data-name", gitButtons[i]);
       btn.text(gitButtons[i]);
       $("#btnDisplay").append(btn);
