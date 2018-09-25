d629a729fb96228c15fdb251ba21355c6ae469a7
SeanB333 <spb4578@gmail.com>
2018-09-25 11:02:24 -0500

    fixing file issues

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,24 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var gitButtons = ["A", "B", "C", "D"];
-    function makeBtns(){
-      $("#btnDisplay").empty();
-      for(i=0;i<gitButtons.length;i++){
-        var btn = $("<button>");
-        btn.addClass("newBtn");
-        btn.attr("data-name", gitButtons[i]);
-        btn.text(gitButtons[i]);
-        $("#btnDisplay").append(btn);
-      }
-    }
+  var gitButtons = ["A", "B", "C", "D"];
 
-    $("#btnAdd").on("click", function(event) {
-      event.preventDefault();
-      var btn = $("#btnDisplay").val().trim();
-      gitButtons.push(btn);
-      makeBtns();
+  function getGiphyInfo() {
+    var search = $(this).attr("data-name");
+    var queryURL = `https://api.giphy.com/v1/gifs/random?api_key=8qTEA6lp3g19Ye08Lcr7r9vanv9kwtLH&tag=${search}&limit=10`;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
     });
+  }
+
+
+  function makeBtns() {
+    $("#btnDisplay").empty();
+    for (i = 0; i < gitButtons.length; i++) {
+      var btn = $("<button>");
+      btn.addClass("newBtn");
+      btn.attr("data-name", gitButtons[i]);
+      btn.text(gitButtons[i]);
+      $("#btnDisplay").append(btn);
+    }
+  }
+
+  $("#addBtn").on("click", function (event) {
+    event.preventDefault();
+    var btn = $("#btnInput").val().trim();
+    gitButtons.push(btn);
+    makeBtns();
+  });
+
+  $(document).on("click", ".newBtn", getGiphyInfo);
   makeBtns();
-    
-    
+
+
 });
